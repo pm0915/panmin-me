1123e8276a6b503a59f6c27eacf5a318bce71708
pm0915 <minpan1206@gmail.com>
2026-02-07 05:11:00 +0800

    refactor: update social links and site title

--- src/constants.ts
+++ src/constants.ts
@@ -2,11 +2,11 @@ import type { Props } from 'astro';
 import IconMail from '@/assets/icons/IconMail.svg';
 import IconGitHub from '@/assets/icons/IconGitHub.svg';
 import IconBrandX from '@/assets/icons/IconBrandX.svg';
-import IconLinkedin from '@/assets/icons/IconLinkedin.svg';
 import IconWhatsapp from '@/assets/icons/IconWhatsapp.svg';
 import IconFacebook from '@/assets/icons/IconFacebook.svg';
 import IconTelegram from '@/assets/icons/IconTelegram.svg';
 import IconPinterest from '@/assets/icons/IconPinterest.svg';
+import IconInstagram from '@/assets/icons/IconInstagram.svg';
 import { SITE } from '@/config';
 
 interface Social {
@@ -18,26 +18,26 @@ interface Social {
 
 export const SOCIALS: Social[] = [
   {
+    name: 'Instagram',
+    href: 'https://www.instagram.com/minpan1206',
+    linkTitle: `${SITE.title} on Instagram`,
+    icon: IconInstagram,
+  },
+  {
     name: 'GitHub',
-    href: 'https://github.com/satnaing/astro-paper',
+    href: 'https://github.com/pm0915',
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
     name: 'X',
-    href: 'https://x.com/username',
+    href: 'https://x.com/minpan1206',
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
-    name: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/username/',
-    linkTitle: `${SITE.title} on LinkedIn`,
-    icon: IconLinkedin,
-  },
-  {
     name: 'Mail',
-    href: 'mailto:yourmail@gmail.com',
+    href: 'mailto:minpan1206@gmail.com',
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
